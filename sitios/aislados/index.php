aa056ed010b60393cb1b4c79f8181538104f47e5
alegreiff <alegreiff@gmail.com>
2014-02-09 23:12:06 -0500

    nueva imagen

--- sitios/aislados/index.php
+++ sitios/aislados/index.php
@@ -139,7 +139,7 @@
         </div>
         <div class="row">
           <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/150/150/sports/1" class="img-thumbnail" alt=""></a><p class="text-center"><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
-          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/150/150/sports/2" class="img-thumbnail" alt=""></a><p class="text-center"><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://placehold.it/150x150" class="img-thumbnail" alt=""></a><p class="text-center"><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
           <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/150/150/sports/3" class="img-thumbnail" alt=""></a><p class="text-center"><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
           <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/150/150/sports/4" class="img-thumbnail" alt=""></a><p class="text-center"><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
           <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/150/150/sports/5" class="img-thumbnail" alt=""></a><p class="text-center"><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
