68b6a545bae26d24c0712fadf709711ae8dd732f
alegreiff <alegreiff@gmail.com>
2014-02-09 23:01:47 -0500

    miniaturas con diferentes clases

--- sitios/aislados/index.php
+++ sitios/aislados/index.php
@@ -138,12 +138,12 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-2 col-xs-3"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a></div>
-          <div class="col-md-2 col-xs-3"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a></div>
-          <div class="col-md-2 col-xs-3"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a></div>
-          <div class="col-md-2 col-xs-3"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a></div>
-          <div class="col-md-2 col-xs-3"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a></div>
-          <div class="col-md-2 col-xs-3"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a></div>
         </div>
         <div class="page-header">
           <h1>recorrido de elementos</h1>
