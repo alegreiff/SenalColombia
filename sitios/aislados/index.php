cbbabd4736fe560144c6a3105ef647327ad65b5e
alegreiff <alegreiff@gmail.com>
2014-02-09 23:06:43 -0500

    Imágenes diversas

--- sitios/aislados/index.php
+++ sitios/aislados/index.php
@@ -138,12 +138,12 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a><p><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
-          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a><p><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
-          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a><p><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
-          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a><p><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
-          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a><p><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
-          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/g/150/150/" class="img-thumbnail" alt=""></a><p><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/150/150/sports/1" class="img-thumbnail" alt=""></a><p><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/150/150/sports/2" class="img-thumbnail" alt=""></a><p><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/150/150/sports/3" class="img-thumbnail" alt=""></a><p><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/150/150/sports/4" class="img-thumbnail" alt=""></a><p><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/150/150/sports/5" class="img-thumbnail" alt=""></a><p><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
+          <div class="col-md-2 col-xs-6 col-sm-4"><a href="#"><img src="http://lorempixel.com/150/150/sports/6" class="img-thumbnail" alt=""></a><p><button type="button" class="btn btn-default btn-xs">Enlace cap.</button></p></div>
         </div>
         <div class="page-header">
           <h1>recorrido de elementos</h1>
