9cbd0047f3da716c08f8b15ba2badddd23a76fc7
Jaime de Greiff <alegreiff@gmail.com>
2014-02-15 09:47:09 -0500

    Seis navegaciones en AISLADOS

--- sitios/aislados/index.php
+++ sitios/aislados/index.php
@@ -81,13 +81,16 @@
                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                     <li data-target="#myCarousel" data-slide-to="1"></li>
                     <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                    <li data-target="#myCarousel" data-slide-to="4"></li>
+                    <li data-target="#myCarousel" data-slide-to="5"></li>
                 </ol>   
                 <!-- Carousel items -->
                 <div class="carousel-inner">
                     <div class="active item">
                         <img src="img/slide01.jpg" alt="Slide 1">
                         <div class="carousel-caption">
-                          <h3>First slide label</h3>
+                          <h3>hdjashdj ag dhg dhjasj</h3>
                           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                         </div>
                     </div>
